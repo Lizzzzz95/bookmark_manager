093becf0a9c6bfa8c55e6f4a218b864403206450
Lizzie Coady <lizziecoady@hotmail.co.uk>
2019-02-28 15:09:15 +0000

    Refactored

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -5,7 +5,6 @@ describe Bookmark do
   describe '.all' do
 
     it 'returns a list of bookmarks' do
-
       connection = PG.connect(dbname: 'bookmark_manager_test')
 
       # Add the test data
@@ -18,7 +17,16 @@ describe Bookmark do
       expect(bookmarks).to include 'http://www.makersacademy.com'
       expect(bookmarks).to include 'http://www.destroyallsoftware.com'
       expect(bookmarks).to include 'http://www.google.com'
+    end
 
+  end
+
+  describe '.create' do
+
+    it 'creates a new bookmark' do
+      Bookmark.create(url: 'http://www.testbookmark.com')
+
+      expect(Bookmark.all).to include 'http://www.testbookmark.com'
     end
 
   end
